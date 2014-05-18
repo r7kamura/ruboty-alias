dd8f42dd4aaef58cfba973188326105160f76b8e
Ryo Nakamura <r7kamura@gmail.com>
2014-05-18 11:50:14 +0900

    Initial commit

---
+++ lib/ellen/alias.rb
@@ -0,0 +1,7 @@
+require "ellen/alias/version"
+
+module Ellen
+  module Alias
+    # Your code goes here...
+  end
+end
---
+++ lib/ellen/alias/version.rb
@@ -0,0 +1,5 @@
+module Ellen
+  module Alias
+    VERSION = "0.0.1"
+  end
+end
