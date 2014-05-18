2fb6312a16c3cb514e2001e7c6c56895e78cddf9
Ryo Nakamura <r7kamura@gmail.com>
2014-05-18 12:00:44 +0900

    Create alias command

--- lib/ellen/handlers/alias.rb
+++ lib/ellen/handlers/alias.rb
@@ -1,6 +1,22 @@
 module Ellen
   module Handlers
     class Alias < Base
+      NAMESPACE = "alias"
+
+      on(
+        /alias (?<from>.+) -> (?<to>.+)\z/,
+        description: "Create alias message",
+        name: "create_alias",
+      )
+
+      # Stores alias in robot's brain
+      def create_alias(message)
+        from = message[:from]
+        to = message[:to]
+        robot.brain.data[NAMESPACE] ||= {}
+        robot.brain.data[NAMESPACE][from] = to
+        message.reply("Registered alias: #{from} -> #{to}")
+      end
     end
   end
 end
